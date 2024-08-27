7afd246ba27e6f412ccdfeac7609c963480a9323
seongeun223 <jklun080921@gmail.com>
2024-08-27 10:32:28 +0900

    router

---
+++ 05_router/01_react-router-config/src/App.js
@@ -0,0 +1,15 @@
+import Menu from "./pages/Menu";
+import Main from "./pages/Main";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <>
+    <Main/>
+    <About/>
+    <Menu/>
+    </>
+  );
+}
+
+export default App; // ⭐
