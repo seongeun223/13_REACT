1b64ea8885656258aec13c466306a192384e38da
seongeun223 <jklun080921@gmail.com>
2024-08-29 10:41:04 +0900

    router

--- 05_router/04_etc/src/pages/Mypage.js
+++ 05_router/04_etc/src/pages/Mypage.js
@@ -5,6 +5,12 @@ function Mypage() {
     const isLogin = false;
 
     if(!isLogin) {
+        /* 
+            컴포넌트 내에서 권한이나 다른 특정 상황에 다른 페이지를 이동하고 싶을 때
+            사용하는 Navigate 컴포넌트
+            - useNavigate와 같지만, 함수식으로 사용할지, 컴포넌트 형태로 사용할지에 따라
+            useNavigate와 Navigate중에 선택 하면 된다.
+        */
         return <Navigate to="/login"/>
     }
 
